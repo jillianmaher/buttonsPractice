d3190af39428f4851167155acca4371123b2083c
Jillian Maher <jillian.maher2@boone.kyschools.us>
2019-12-11 08:31:56 -0500

    Update buttons

---
+++ script.js
@@ -0,0 +1,7 @@
+var question = "Which of the following is a school color?";
+
+document.getElementById("question").innerHTML = question;
+
+function changeColor() {
+    document.getElementById('demo2').setAttribute('style','background-color:rgb(215,50,35);');
+}
