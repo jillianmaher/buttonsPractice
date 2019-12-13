978f80777aefc4d28246319c0400598059adf921
Jillian Maher <jillian.maher2@boone.kyschools.us>
2019-12-13 11:38:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-var question = "Which of the following is a school color?";
-var answers = ['Red', 'Green', 'Blue', 'Yellow'];
+var question = ["Which of the following is a school color?",
+                "Question 2"];
+var answers = [  ['Red', 'Green', 'Blue', 'Yellow'], 
+                ['One', 'Two', 'Three', 'Four'] ];
 var answerButtons = document.getElementsByClassName("quizButton");
-var correct = 3;
+var correct = [3, 1];
+var currQ = 0;
 
-document.getElementById("question").innerHTML = question;
+document.getElementById("question").innerHTML = question[currQ];
 
-for(var spot=0; spot<answers.length; spot++)
+for(var spot=0; spot<answers[currQ].length; spot++)
 {
-    answerButtons[spot].innerHTML = answers[spot];
+    answerButtons[spot].innerHTML = answers[currQ][spot];
 
     answerButtons[spot].addEventListener('click', function() {
         disableAll();
-        if(this.innerHTML == answers[correct]){
+        if(this.innerHTML == answers[currQ][correct[currQ]]){
             this.setAttribute('style','background-color:rgb(76,175,80);');
         }else {
             this.setAttribute('style','background-color:rgb(215,50,35);');
